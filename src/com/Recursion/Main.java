dcef92c488a7c74f81e577d822df37cd13030bc7
Nitin <nsinghboss02@gmail.com>
2022-02-02 23:25:20 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,25 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	skip "apple" ina given string.
+	 */
+        String str = "bccapplenit";
+        System.out.println(skipApple(str));
+
+    }
+    public static String skipApple(String str){
+        if(str.isEmpty()){
+            return " ";
+        }
+        if(str.startsWith("apple")){
+            return skipApple(str.substring(5));
+        }
+        else{
+            return str.charAt(0)+skipApple(str.substring(1));
+        }
+    }
+
+}
